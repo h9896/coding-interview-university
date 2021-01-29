414fe8de1d295209fe6e5abe2eea1e8c1485a03a
宋憲霖 <songxianlin@songxianlindeMacBook-Air.local>
2021-01-29 16:13:53 +0800

    Add point update in binary index tree and add complexity

--- practice/dataStructures/fenwickTree.py
+++ practice/dataStructures/fenwickTree.py
@@ -3,7 +3,10 @@ class FewnickTree:
         self.data = array
         self.size = len(array)
         self.tree = self.construction()
-    def construction(self) -> [int]:      
+    def construction(self) -> [int]:
+        '''
+        complexity: O(n)
+        '''
         array = [0]
         array = array + self.data.copy()
         for i in range(1, self.size+1):
@@ -14,12 +17,25 @@ class FewnickTree:
     def prefix_sum(self, index: int) -> int:
         '''
         the index starts with 1 not 0
+        complexity: O(log(n))
         '''
         result = 0
         while index != 0:
             result = result + self.tree[index]
             index = index - self.lsb(index)
         return result
+    def point_update(self, index: int, val: int):
+        '''
+        the index starts with 1 not 0
+        complexity: O(log(n))
+        '''
+        if index <= 0 or index > self.size:
+            raise IndexError("The index out of range")
+        add_val = val - self.tree[index]
+        while index < self.size+1:
+            self.tree[index] = self.tree[index] + add_val
+            index = index + self.lsb(index)
+        self.data[index - 1] = val
     def range_sum_query(self, start: int, end: int) -> int:
         return self.prefix_sum(end) - self.prefix_sum(start)
     def lsb(self, index: int) -> int:
